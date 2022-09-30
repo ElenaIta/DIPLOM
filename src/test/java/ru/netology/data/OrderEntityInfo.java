5ea44fc0b363b08e0e7fc1504b4f48ee553ca28c
Elena_An <e.anikey@gmail.com>
2022-09-30 23:37:52 +0300

    add srs

---
+++ src/test/java/ru/netology/data/OrderEntityInfo.java
@@ -0,0 +1,16 @@
+package ru.netology.data;
+
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+
+public class OrderEntityInfo {
+
+    private String id;
+    private String created;
+    private String credit_id;
+    private String payment_id;
+}
